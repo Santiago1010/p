52671e55b3f6ddba27a8e62cdd04cb052abb2775
cdlavacudeg <cristianlav10@gmail.com>
2023-01-25 15:13:09 -0500

    [Apply] Prettier Lint

--- validateJwt.js
+++ validateJwt.js
@@ -1,33 +1,33 @@
 const jwt = require('jsonwebtoken');
-const initModels = require('../models/db/init-models')
-const { sequelize } = require('../database/config')
-const { usuarios } = initModels(sequelize)
+const initModels = require('../models/db/init-models');
+const { sequelize } = require('../database/config');
+const { usuarios } = initModels(sequelize);
 
 const validateJwt = async (req, res, next) => {
-  const token = req.header('Authorization')
+  const token = req.header('Authorization');
   if (!token) {
     return res.status(401).send({
-      msg: 'token requerido - 1'
-    })
+      msg: 'token requerido - 1',
+    });
   }
   try {
-    const { id } = jwt.verify(token, process.env.PRIVATEKEY)
-    const usuario = await usuarios.findOne({ where: { id } })
+    const { id } = jwt.verify(token, process.env.PRIVATEKEY);
+    const usuario = await usuarios.findOne({ where: { id } });
     if (!usuario) {
       return res.status(401).send({
-        msg: 'token no valido - 2'
-      })
+        msg: 'token no valido - 2',
+      });
     }
-    req.user = usuario
-    next()
+    req.user = usuario;
+    next();
   } catch (error) {
     console.log(error);
     res.status(401).send({
-      msg: 'token no valido - 3'
-    })
+      msg: 'token no valido - 3',
+    });
   }
-}
+};
 
 module.exports = {
-  validateJwt
-}
+  validateJwt,
+};
--- validatePermission.js
+++ validatePermission.js
@@ -1,22 +1,21 @@
-const initModels = require('../models/db/init-models')
-const { sequelize } = require('../database/config')
-const { usuarios_permisos_opciones, opciones } = initModels(sequelize)
-const { validarOpcion } = require('../helpers/dbValidators')
-
+const initModels = require('../models/db/init-models');
+const { sequelize } = require('../database/config');
+const { usuarios_permisos_opciones, opciones } = initModels(sequelize);
+const { validarOpcion } = require('../helpers/dbValidators');
 
 const validatePermission = async (req, res, next) => {
-    // validar si el usuario tiene permiso
-    const { id, nombre } = req.user;
-    let { method, path } = req.body;
-    if (!method && !path) {
-        method = req.method;
-        path = req.path;
-    }
+  // validar si el usuario tiene permiso
+  const { id, nombre } = req.user;
+  let { method, path } = req.body;
+  if (!method && !path) {
+    method = req.method;
+    path = req.path;
+  }
 
-    console.log("validando", path);
-    const permiso = await validarOpcion(path, method, id);
-    
-    /*
+  console.log('validando', path);
+  const permiso = await validarOpcion(path, method, id);
+
+  /*
     usuarios_permisos_opciones.findOne({
         where: { id_usuario: id, metodo: method },
         include: [
@@ -33,15 +32,15 @@ const validatePermission = async (req, res, next) => {
     })
     */
 
-    if (!permiso) {
-        return res.status(400).json({
-            msg: `${nombre}, usted no tiene permiso para realizar esta acción`
-        })
-    }
+  if (!permiso) {
+    return res.status(400).json({
+      msg: `${nombre}, usted no tiene permiso para realizar esta acción`,
+    });
+  }
 
-    next()
-}
+  next();
+};
 
 module.exports = {
-    validatePermission
-}
+  validatePermission,
+};
