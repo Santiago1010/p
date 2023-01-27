b98e8e90ad84151e3741c11d565079dbbfa622e2
cdlavacudeg <cristianlav10@gmail.com>
2023-01-27 10:46:34 -0500

    [Update] Response errores express-validator

--- validateFields.js
+++ validateFields.js
@@ -1,11 +1,15 @@
 const { validationResult } = require('express-validator');
+const response = require('./../helpers/response');
 
 const validateFields = (req, res, next) => {
-  const errors = validationResult(req);
+  const errorFormatter = ({ location, msg, param }) => {
+    return `${location}[${param}]: ${msg}`;
+  };
+  const errors = validationResult(req).formatWith(errorFormatter);
   if (!errors.isEmpty()) {
-    return res.status(400).json(errors);
+    return response.error(req, res, errors.array().join(';'), 400);
   }
-  next();
+  return next();
 };
 
 module.exports = {
