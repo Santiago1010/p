1a350157fb3bc4e235233dd2fc3cc80f25f7428f
cdlavacudeg <cristianlav10@gmail.com>
2023-01-31 12:39:52 -0500

    [Update] Lógica lista empresas

---
+++ pagination.js
@@ -0,0 +1,38 @@
+const error = require('./customError');
+class Paginate {
+  static async paginateModel(model, limit, page, { attributes, where, order } = {}) {
+    const pageQuery = page - 1;
+    const offset = page && limit ? pageQuery * limit : 0;
+    const limitQuery = limit ? parseInt(limit) : undefined;
+
+    if (page && !limit) {
+      throw error('Si se envia page, tambien se debe enviar limit', 400);
+    }
+
+    const optionQuery = {
+      offset,
+      limit: limitQuery,
+      attributes,
+      where,
+      order,
+    };
+
+    const { rows: results, count } = await model.findAndCountAll(optionQuery);
+
+    const limitAndPage = page && limit;
+
+    const existPrev = limitAndPage ? page > 1 && offset < count : null;
+    const existNext = limitAndPage ? Math.floor(count / limit) > page : null;
+
+    return { total: count, results, existPrev, existNext, page };
+  }
+
+  static async paginateReqs(existNext, existPrev, page, req) {
+    const pagina = parseInt(page);
+    const prev = existPrev ? `${req.protocol}://${req.get('host')}${req.baseUrl}?page=${pagina - 1}` : null;
+    const next = existNext ? `${req.protocol}://${req.get('host')}${req.baseUrl}?page=${pagina + 1}` : null;
+    return { prevPage: prev, nextPage: next };
+  }
+}
+
+module.exports = Paginate;
