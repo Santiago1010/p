2b04779026445c262b756f980b71a48a2a05b12e
cdlavacudeg <cristianlav10@gmail.com>
2023-03-22 17:37:38 -0500

    [Update] Mensaje en creación de usuarios
    Mensaj mas organizado y nuevo response con mensaje

--- validateFields.js
+++ validateFields.js
@@ -12,6 +12,20 @@ const validateFields = (req, res, next) => {
   return next();
 };
 
+const validateFieldsPretty = (req, res, next) => {
+  const errorFormatter = ({ msg, param }) => {
+    const newParam = param.split('[')[0].replace('os', 'o');
+    const number = param.match(/\[(\d+)\]/);
+    return `${newParam}${number ? `-${parseInt(number[1]) + 1}` : ''}: ${msg}`;
+  };
+  const errors = validationResult(req).formatWith(errorFormatter);
+  if (!errors.isEmpty()) {
+    return response.errorWithMesage(req, res, errors.array(), 'Existen errores en los datos ingresados', 400);
+  }
+  return next();
+};
+
 module.exports = {
   validateFields,
+  validateFieldsPretty,
 };
