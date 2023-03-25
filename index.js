b38def563f176fb9d80795bf1f5d327aaf7c8daa
cdlavacudeg <cristianlav10@gmail.com>
2023-03-25 09:13:05 -0500

    [Update] Creación parrafo leccion
    Modificación de modelo tabla lecciones cursos para detalle de lección, y validaciones avanzadas del body para asegurar que se envien los datos adecuados

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const paginationSchema = require('./paginationSchema');
+const helpersSchemas = require('./helpersSchemas');
 const databaseSchemas = require('./databaseSchemas');
 
-module.exports = { paginationSchema, databaseSchemas };
+module.exports = { paginationSchema, databaseSchemas, helpersSchemas };
