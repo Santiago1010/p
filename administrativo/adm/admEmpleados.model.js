7b8732b12fcd0ccf089b0711e6862aee5489d5a1
Cristian David Lavacude Galvis <cristianlav10@gmail.com>
2023-07-26 14:31:49 +0000

    Bn 147 rol dinamico

--- administrativo/adm/admEmpleados.model.js
+++ administrativo/adm/admEmpleados.model.js
@@ -60,15 +60,7 @@ const Schema = {
     type: DataTypes.STRING(12),
     allowNull: false,
   },
-  passemp: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
   diremp: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  sdapass: {
     type: DataTypes.TEXT,
     allowNull: false,
   },
@@ -79,10 +71,6 @@ const Schema = {
       model: 'adm_cargos',
       key: 'codcrg',
     },
-  },
-  nomcargo: {
-    type: DataTypes.STRING(300),
-    allowNull: false,
   },
   fotusr: {
     type: DataTypes.TEXT,
@@ -95,50 +83,6 @@ const Schema = {
       }
       return `${hostImage}${imageLocation}`;
     },
-  },
-  prfemp: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    comment: 'perfil',
-    references: {
-      model: 'usuarios_perfil',
-      key: 'id',
-    },
-  },
-  dirgrp: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  },
-  stdemp: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-    defaultValue: 'ACTIVO',
-    comment: 'estado',
-  },
-  codinf: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-    comment: 'codigo verificacion',
-  },
-  sednom: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    comment: 'sede razon social',
-  },
-  depuser: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'adm_dependencias',
-      key: 'coddep',
-    },
-  },
-  nivEscolar: {
-    type: DataTypes.ENUM('Ninguno', 'Preescolar', 'Primaria', 'Secundaria & Media'),
-    allowNull: false,
-    field: 'niv_escolar',
   },
   eps: {
     type: DataTypes.STRING(50),
@@ -156,11 +100,6 @@ const Schema = {
       key: 'id',
     },
   },
-  nomCuenta: {
-    type: DataTypes.STRING(50),
-    allowNull: true,
-    field: 'nom_cuenta',
-  },
   tipoCuenta: {
     type: DataTypes.STRING(50),
     allowNull: false,
@@ -171,54 +110,13 @@ const Schema = {
     allowNull: false,
     field: 'nro_cuenta',
   },
-  ctbedt: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-    comment: 'Editar procesos contables',
-  },
   updusr: {
     type: DataTypes.STRING(15),
     allowNull: false,
   },
-  fchupd: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-  },
-  entrevista: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-    comment: 'Realiza entrevista',
-  },
-  updestudy: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  },
   ultfirma: {
     type: DataTypes.STRING(80),
     allowNull: false,
-  },
-  evalucion: {
-    type: DataTypes.ENUM('Empleado', 'Docente', 'General'),
-    allowNull: false,
-    defaultValue: 'Empleado',
-  },
-  contratos: {
-    type: DataTypes.STRING(50),
-    allowNull: true,
-    references: {
-      model: 'adm_empleados_contrato',
-      key: 'codcontrato',
-    },
-  },
-  revisarProyectos: {
-    type: DataTypes.STRING(80),
-    allowNull: false,
-    comment: 'revisar_test',
-    field: 'revisar_proyectos',
   },
   paisNacimiento: {
     type: DataTypes.INTEGER,
