e209fd68e82d3d5edbe05addd85c8b156bfaf586
Cristian David Lavacude Galvis <cristianlav10@gmail.com>
2023-09-14 15:51:31 +0000

    [Feature] Matricula - Contratos

--- validateFields.js
+++ validateFields.js
@@ -1,9 +1,11 @@
 const { validationResult } = require('express-validator');
 const response = require('./../helpers/response');
 
+const { env } = require('../config');
+
 const validateFields = (req, res, next) => {
   const errorFormatter = ({ location, msg, param }) => {
-    return `${location}[${param}]: ${msg}`;
+    return env == 'production' ? msg : `${location}[${param}]: ${msg}`;
   };
   const errors = validationResult(req).formatWith(errorFormatter);
   if (!errors.isEmpty()) {
@@ -12,12 +14,21 @@ const validateFields = (req, res, next) => {
   return next();
 };
 
-const validateFieldsPretty = (req, res, next) => {
+const validateFieldsRegistroUsuarios = (req, res, next) => {
+  const { usuarios } = req.body;
+
+  if (!usuarios || usuarios?.length == 0) {
+    return response.error(req, res, 'No se ingresaron usuarios', 400);
+  }
+
   const errorFormatter = ({ msg, param }) => {
     const newParam = param.split('[')[0].replace('os', 'o');
     const number = param.match(/\[(\d+)\]/);
-    return `${newParam}${number ? `-${parseInt(number[1]) + 1}` : ''}: ${msg}`;
+    const offset = usuarios.length == 1 ? 1 : 2;
+
+    return offset == 1 ? `${msg}` : `${newParam}${number ? `-${parseInt(number[1]) + offset}: ` : ': '}${msg}`;
   };
+
   const errors = validationResult(req).formatWith(errorFormatter);
   if (!errors.isEmpty()) {
     return response.errorWithMesage(req, res, errors.array(), 'Existen errores en los datos ingresados', 400);
@@ -27,5 +38,5 @@ const validateFieldsPretty = (req, res, next) => {
 
 module.exports = {
   validateFields,
-  validateFieldsPretty,
+  validateFieldsRegistroUsuarios,
 };
